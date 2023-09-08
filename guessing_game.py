e06dfa97aa5e14c6962b474e04563fbdc70c05cf
Jacob D <35861176+JakeDLL@users.noreply.github.com>
2023-09-07 21:11:30 -0400

    Update guessing_game.py
    removed extra comments and instructions

--- guessing_game.py
+++ guessing_game.py
@@ -7,22 +7,6 @@ Project 1 - The Number Guessing Game
 import random
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
     # This is the welcome message with instructions as to how to play the game.
     print("""
                            (0)
@@ -97,27 +81,3 @@ def start_game():
 # Kick off the program by calling the start_game function.
 start_game()
 print('Thank you so much for playing my game!')
-
-
-
-# Import the random module.
-
-# Create the start_game function.
-# Write your code inside this function.
-
-#   When the program starts, we want to:
-#   ------------------------------------
-#   1. Display an intro/welcome message to the player.
-#   2. Store a random number as the answer/solution.
-#   3. Continuously prompt the player for a guess.
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
-
-#   4. Once the guess is correct, stop looping, inform the user they "Got it"
-#      and show how many attempts it took them to get the correct number.
-#   5. Let the player know the game is ending, or something that indicates the game is over.
-
-# ( You can add more features/enhancements if you'd like to. )
-
-
-# Kick off the program by calling the start_game function.
